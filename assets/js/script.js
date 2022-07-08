3449ccb5e36292eca19966043b8027b38bb18e31
Sabirul Islam <45106497+mdsabir07@users.noreply.github.com>
2022-07-08 13:12:24 +0600

    commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+(function($){
+    "use strict";
+
+    $(".msitheme-slider").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+
+})(jQuery);
